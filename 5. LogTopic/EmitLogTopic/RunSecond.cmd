2583c25c90c4a3b4e55bd59c048438a1871fb63f
Ibelyaev <Ibelyaev@DESKTOP>
2020-12-29 13:52:52 +0500

    Topic

---
+++ 5. LogTopic/EmitLogTopic/RunSecond.cmd
@@ -0,0 +1,7 @@
+dotnet run "kern.critical" "A critical kernel error"
+
+dotnet run "kern.info" "An informational kernel message"
+
+dotnet run "message.critical" "A critical error from messaging system"
+
+dotnet run "message.info" "Info log from message system"
